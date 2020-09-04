6e4c0d2876579330b6cfab3decfdf35ec36cdc48
Documenter.jl <documenter@juliadocs.github.io>
2020-09-04 23:14:37 +0000

    build based on fbc5d04

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SossExamples","category":"page"},{"location":"#SossExamples","page":"Home","title":"SossExamples","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This repository contains additional examples for the  Soss.jl package.","category":"page"}]
+}
